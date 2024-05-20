866a2024168b499e39465c4b02fe12a7ab552e41
lmfa-cap <luis-miguel.almeida@capgemini.com>
2024-05-20 10:17:19 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -32,9 +32,3 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
   console.log('Server is running on http://localhost:3000/');
 });
-// The comments.js file exports an array of comments. The server reads comments from the file and sends them back to the client when the client makes a GET request to /comments. When the client sends a POST request to /comments, the server reads the request body, parses it, and adds the new comment to the comments array. Finally, the server sends the new comment back to the client.
-
-// Run the server using node comments.js and test it using a tool like Postman. You can make GET requests to /comments to read comments and POST requests to /comments to add new comments.
-
-// Conclusion
-// In this article, you learned how to create a simple web server using Node.js. You also learned how to handle different types of HTTP requests (GET, POST, etc.) and send responses to clients. You can use this knowledge to build more complex web servers that serve static files, handle form submissions, authenticate users, and more. I hope this article helps you get started with Node.js web development!
